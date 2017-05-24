f14785ac68c9c81aa97c582b476d001f399d00bc
ProgFroz <justin.nicola@gmx.net>
2017-05-24 13:31:34 +0200

    Added Food as class

---
+++ src/Food.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Chrono on 22.05.2017.
+ */
+public class Food {
+    private int f_id;
+    private int amount;
+    private int dangerous; //0 - not dangerous, 1 - spikes, 2 - poison, 3 - radioactive ?
+    private int[] position;
+
+
+
+}
